e9b641912898d51615947379b39736c4fedaded7
sakda homhuan <sakda.homhuan@gmail.com>
2018-05-25 22:36:52 +0700

    add first

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ReportProvider } from '../providers/report/report';
+import { DetailPage } from '../pages/detail/detail';
+import { ListPage } from '../pages/list/list';
+import { ReportPage } from '../pages/report/report';
+import { HttpClientModule } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Camera } from '@ionic-native/camera';
+import { LayerPage } from '../pages/layer/layer';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    DetailPage,
+    ListPage,
+    ReportPage,
+    LayerPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    DetailPage,
+    ListPage,
+    ReportPage,
+    LayerPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    Camera,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ReportProvider
+  ]
+})
+export class AppModule { }
